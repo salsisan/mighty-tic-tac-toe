849909cad493daf62e0d680957e6020cb49b513b
yelsayed <yasser.ebrahim@gmail.com>
2014-10-13 03:44:06 -0700

    shwayet files ma3rafsh homa eh

---
+++ Mighty Tick Tac Toe/Mighty Tick Tac Toe.Shared/GameEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mighty_Tick_Tac_Toe
+{
+    enum MoveState
+    {
+        ERROR_CELL_FULL,
+        ERROR_WRONG_TURN,
+        ERROR_WRONG_BOARD,
+
+        SUCCESS,
+        SUCCESS_BOARD_WON,
+        SUCCESS_BOARD_DRAW,
+        SUCCESS_GAME_WON,
+        SUCCESS_GAME_DRAW
+    }
+
+    public class GameEngine
+    {
+        //MoveState PlayMove(int player, int bx, int by, int cx, int cy);
+    }
+}
