d7ccd76ca3f4c47ba05da9ee2d56cae9fb42dc07
yelsayed <yasser.ebrahim@gmail.com>
2014-10-13 03:43:34 -0700

    adding GameEngine with a grid

--- Mighty Tick Tac Toe/Mighty Tick Tac Toe.Windows/StartPage.xaml.cs
+++ Mighty Tick Tac Toe/Mighty Tick Tac Toe.Windows/StartPage.xaml.cs
@@ -20,9 +20,9 @@ namespace Mighty_Tick_Tac_Toe
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class StartPage : Page
     {
-        public MainPage()
+        public StartPage()
         {
             this.InitializeComponent();
         }
--- Mighty Tick Tac Toe/Mighty Tick Tac Toe.WindowsPhone/GamePage.xaml.cs
+++ Mighty Tick Tac Toe/Mighty Tick Tac Toe.WindowsPhone/GamePage.xaml.cs
@@ -13,20 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace Mighty_Tick_Tac_Toe
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class GamePage : Page
     {
-        public MainPage()
+        public GamePage()
         {
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
         /// <summary>
@@ -36,13 +34,6 @@ namespace Mighty_Tick_Tac_Toe
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
         }
     }
 }
